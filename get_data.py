139e4b840c0e81872f6d8785096ee6adb3c833a4
Kiril Piskunov <ciudilo@gmail.com>
2013-07-15 06:37:27 +0100

    pipe separated values for initial data

--- get_data.py
+++ get_data.py
@@ -11,20 +11,21 @@ def get_stage_results(stage):
 	
 	soup = BeautifulSoup(r.text)
 
-	f=open (stage+'.txt' , 'a')
-	for tr in soup.find('tbody').find_all('tr'):
-		rank = tr.find('td').text
-		country = tr.find('span').text
-		rider_link = tr.find_all('a')[0]['href']
-		rider = tr.find_all('a')[0].text
-		team_link = tr.find_all('a')[1]['href']
-		team = tr.find_all('a')[1].text
-		gap = tr.find('td', attrs={'class' : 'col_5'}).text
-		total_time = tr.find('td', attrs={'class' : 'col_5'}).findNextSibling('td').text
+	with open(stage+'.txt' , 'a') as f:
+		for tr in soup.find('tbody').find_all('tr'):
+			rank = tr.find('td').text
+			country = tr.find('span').text
+			rider_link = tr.find_all('a')[0]['href']
+			rider = tr.find_all('a')[0].text
+			rider_no = tr.find('td', attrs={'class' : 'col_3'}).text
+			team_link = tr.find_all('a')[1]['href']
+			team = tr.find_all('a')[1].text
+			total_time = tr.find('td', attrs={'class' : 'col_5'}).text
+			gap = tr.find('td', attrs={'class' : 'col_5'}).findNextSibling('td').text
   		
-		parsed = rank + " " + country + " " + rider_link + " " + rider + " " + team_link + " " + team + " " + gap + " " + total_time
-		
-		print( parsed.encode('ascii', 'ignore'), file=f)
+			parsed = rank + "|" + country + "|" + rider_link + "|" + rider + "|" + rider_no + "|" + team_link + "|" + team + "|" + gap + "|" + total_time
+	
+			print( parsed.encode('ascii', 'ignore'), file=f)
 
-for i in range(1,15):
+for i in range(1,16):
 	get_stage_results(str(i))
