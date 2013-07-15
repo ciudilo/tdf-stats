5c60990c6f0b6b05a2a67e2d7edcb592b4b21e1e
Kiril Piskunov <ciudilo@gmail.com>
2013-07-15 06:34:25 +0100

    parse_data now produces csv file in correct format

--- parse_data.py
+++ parse_data.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 
 import csv
 
@@ -16,12 +17,31 @@ def put_gaps(filename):
                 csvreader = csv.DictReader(csvfile, fieldnames=columns, delimiter='|')
                 for row in csvreader:
                         gap = row['gap'].replace("+","").replace("'", "").replace(" ", "").replace("h","")
+			gap = parse_number(gap)
 			rider =  row['rider']
                         table[rider].append(gap)
+def parse_number(num_str):
+	num_str = num_str.strip()
+	if len(num_str) > 0:
+		num_str = str(int(num_str))
+	else:
+		num_str = '0'
+
+	return num_str
+
+
+def write_result():
+	with open('itg.csv', 'a') as csvresult:
+		csvwriter = csv.writer(csvresult, delimiter=',')
+		csvwriter.writerow(['Rider'] + range(1,16))
+		for key in table:
+			csvwriter.writerow([key] + table[key])
 
 init_dict()
 
 for i in range(1,16):
 	put_gaps(str(i)+'.txt')
+
+write_result()
 
 print table
